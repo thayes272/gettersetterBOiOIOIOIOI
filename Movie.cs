894f0c61382c30ec7f28f248fc413378474151e8
Dax272 <49327226+Dax272@users.noreply.github.com>
2021-07-26 23:45:06 -0700

    uploading mainframe
    yer

---
+++ Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getterSetterSHIT
+{
+    class Movie
+    {
+        public string Title;
+        private string rating;
+        public int seconds;
+        public Movie(string aTitle, string aRating, int secondsTotal)
+        {
+            Title = aTitle;
+            Rating = aRating;
+            seconds = secondsTotal;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "R" || value == "PG-13" || value == "PG" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace getterSetterSHIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Movie shrek = new Movie("Shrek", "PG-13", 160);
+            //Console.WriteLine(shrek.Rating);
+            Console.WriteLine("Whats your movie title?");
+            var movieTitle = Console.ReadLine();
+
+            Console.WriteLine("Whats your movie rating?");
+            var movieRating = Console.ReadLine();
+
+            Console.WriteLine("Whats your movie in seconds?");
+            var movieSeconds = Console.ReadLine();
+            
+            Movie testMovie = new Movie(movieTitle, movieRating, (int)double.Parse(movieSeconds));
+            Console.WriteLine(testMovie.Title + " " + testMovie.Rating + " " + testMovie.seconds);
+
+        }
+    }
+}
